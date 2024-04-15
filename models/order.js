9dded91b452046528f7fca87a00fd407a66021e3
Stewpra <wilbstewart7@gmail.com>
2024-04-15 19:05:30 -0400

    added order model

---
+++ models/order.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Order extends Model {}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    order_item_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'orderItem',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'order',
+  },
+);
+
+module.exports = Order;
