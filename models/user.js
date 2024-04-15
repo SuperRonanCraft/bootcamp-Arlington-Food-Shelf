7a502bd6e45cb605050c4c69f3404beb468ea83b
Pema Sherpa <pematamang111@gmail.com>
2024-04-15 13:19:31 -0400

    add email property

--- models/user.js
+++ models/user.js
@@ -12,5 +12,17 @@ User.init({
     autoIncrement: true,
     primaryKey: true,
  },
- 
+ name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+},
+email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+        isEmail: true,
+    }
+},
+
 });
