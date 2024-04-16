1b87828684f5d31ec8c71088ef9c1cc5fe68eb44
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-16 13:27:13 -0400

    added comments to the signup function

--- public/js/login.js
+++ public/js/login.js
@@ -24,19 +24,21 @@ const login = async (event) => {
 //Signup Event
 const signup = async (event) => {
   event.preventDefault();
-
+  // Collect values from the signup form
   const name = document.querySelector('#signup-name').value.trim();
   const email = document.querySelector('#signup-email').value.trim();
   const password = document.querySelector('#signup-password').value.trim();
-
+  // Send a POST request to the API endpoint
   if (name && password && email) {
     const response = await fetch('/api/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, email, password }),
     });
+    // If successful, redirect the browser to the homepage
     if (response.ok) {
       document.location.replace('/');
+      // If not successful, alert the user
     } else {
       alert('Error occured creating account!');
     }
