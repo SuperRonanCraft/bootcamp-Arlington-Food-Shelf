6cd81488241763c0593f30130af92254f8ca689e
Stewpra <wilbstewart7@gmail.com>
2024-04-16 11:16:43 -0400

    cat model

---
+++ models/catagory.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Catagoty extends Model {}
+
+Catagoty.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'inventory',
+  },
+);
