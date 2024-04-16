061ecc5f667baabde32580c828029fbafca5f7d2
RonanCraft <SuperRonanCraft@gmail.com>
2024-04-16 19:47:14 -0400

    order route

--- controllers/api/orderRoute.js
+++ controllers/api/orderRoute.js
@@ -1,4 +1,23 @@
 const router = require('express').Router();
-// const { Inventory } = require('../../models/index');
+const { Order } = require('../../models/index');
+
+router.get('/', (req, res) => {
+  Order.findAll().then((data) => {
+    const orders = data.map((obj) => obj.get({ plain: true }));
+    console.log(orders);
+    res.json(orders);
+  });
+});
+
+router.post('/', (req, res) => {
+  const { order_data } = req.body;
+  console.log(order_data);
+  Order.cre;
+  //   Order.findAll().then((data) => {
+  //     const orders = data.map((obj) => obj.get({ plain: true }));
+  //     console.log(orders);
+  //     res.json(orders);
+  //   });
+});
 
 module.exports = router;
