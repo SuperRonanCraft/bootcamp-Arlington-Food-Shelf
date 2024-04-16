0737b7b4e9d70862b33aa679d2b92bac09e31545
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-16 13:17:56 -0400

    converted login to async await

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,27 @@
 //Login Event
-function login(event) {
+const login = async (event) => {
   event.preventDefault();
 
   const email = document.querySelector('#login-email').value.trim();
   const password = document.querySelector('#login-password').value.trim();
 
   if (email && password) {
-    fetch('/api/login', {
+    const response = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        if (data.loggedIn) {
-          document.location.replace('/');
-        } else {
-          alert('Failed to login!');
-        }
-      });
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to login!');
+    }
   }
-}
+};
 
 //Signup Event
-function signup(event) {
+const signup = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#signup-name').value.trim();
@@ -33,24 +29,18 @@ function signup(event) {
   const password = document.querySelector('#signup-password').value.trim();
 
   if (name && password && email) {
-    console.log('test');
-    fetch('/api/signup', {
+    const response = await fetch('/api/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name, email, password }),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        if (data.loggedIn) {
-          document.location.replace('/');
-        } else {
-          alert('Error occured creating account!');
-        }
-      });
+    });
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Error occured creating account!');
+    }
   }
-}
+};
 
 //User Interaction
 document.querySelector('#button-signup').addEventListener('submit', signup);
