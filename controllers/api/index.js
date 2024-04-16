862dd4728c3305c69081637dff236c5fb25a1d45
RonanCraft <SuperRonanCraft@gmail.com>
2024-04-16 11:37:18 -0400

    signup route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 //User Routes
 router.use('/login', require('./loginRoute'));
 router.use('/logout', require('./logoutRoute'));
+router.use('/signup', require('./signupRoute'));
 
 //Data Routes
 
