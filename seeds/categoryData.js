f1ccee6c074035234c1d99243cb3cc5b68709d08
Stewpra <wilbstewart7@gmail.com>
2024-04-16 19:40:28 -0400

    category seeds

---
+++ seeds/categoryData.js
@@ -0,0 +1,12 @@
+const { Catagory } = require('../models/index');
+
+const data = [
+  { name: 'produce' },
+  { name: 'meat' },
+  { name: 'dairy' },
+  { name: 'bread' },
+  { name: 'canned goods' },
+  { name: 'snacks' },
+];
+
+module.exports = { seedCat };
