d836d17c5ac243fd139075c20ec09469d6edffe4
RonanCraft <SuperRonanCraft@gmail.com>
2024-04-17 13:43:44 -0400

    fixed resource data

---
+++ assets/schedule.js
@@ -0,0 +1,40 @@
+module.exports = {
+  schedule: [
+    {
+      day: 'Monday',
+      times: ['1st Monday 1:30-4 pm', '3rd Monday 3:30-5 pm'],
+      additionalSources: [
+        {
+          name: '3SquaresVT',
+          description: 'Helps you buy food from grocery...',
+        },
+        {
+          name: 'Meals for Kids',
+          description: 'Summer meal sites serve free meals...',
+        },
+        {
+          name: 'The Take Care Project',
+          description: 'Expands access to free hygiene products...',
+        },
+      ],
+    },
+    {
+      day: 'Tuesday',
+      times: ['1st Tuesday 1:30-4 pm', '3rd Tuesday 3:30-5 pm'],
+      additionalSources: [
+        {
+          name: '3SquaresVT',
+          description: 'Helps you buy food from grocery...',
+        },
+        {
+          name: 'Meals for Kids',
+          description: 'Summer meal sites serve free meals...',
+        },
+        {
+          name: 'The Take Care Project',
+          description: 'Expands access to free hygiene products...',
+        },
+      ],
+    },
+  ],
+};
