12f6a0b738a9394912176b63415e8ddeee1c8080
Nora Shala <norashala00@gmail.com>
2024-04-17 14:54:44 -0400

    resources changes

--- assets/schedule.js
+++ assets/schedule.js
@@ -1,40 +1,23 @@
 module.exports = {
   schedule: [
     {
-      day: 'Monday',
-      times: ['1st Monday 1:30-4 pm', '3rd Monday 3:30-5 pm'],
-      additionalSources: [
-        {
-          name: '3SquaresVT',
-          description: 'Helps you buy food from grocery...',
-        },
-        {
-          name: 'Meals for Kids',
-          description: 'Summer meal sites serve free meals...',
-        },
-        {
-          name: 'The Take Care Project',
-          description: 'Expands access to free hygiene products...',
-        },
-      ],
-    },
-    {
       day: 'Tuesday',
       times: ['1st Tuesday 1:30-4 pm', '3rd Tuesday 3:30-5 pm'],
       additionalSources: [
         {
           name: '3SquaresVT',
-          description: 'Helps you buy food from grocery...',
+          description: '3SquaresVT helps you buy food from grocery and convenience stores, farmers markets, and co-ops. 3SquaresVT benefits are loaded each month on an EBT card, which works like a debit card. To find out more and get help applying visit VermontFoodHelp.com or text VFBSNAP to 85511.',
         },
         {
           name: 'Meals for Kids',
-          description: 'Summer meal sites serve free meals...',
+          description: 'Summer meal sites across the state serve free nutritious meals to kids, 0-18. To find meal locations open to all kids near you, view this sheet of Open Meal Site Locations or the USDA Summer Meal Map.',
         },
         {
           name: 'The Take Care Project',
-          description: 'Expands access to free hygiene products...',
+          description: 'The Take Care Project is on a mission to expand access to free period products and hygiene essentials in the Bennington region. Explore the Care Map for locations and organizations in Bennington that offer free personal care items including tampons, pads, shampoo, soap, and deodorant.',
         },
       ],
     },
+  
   ],
 };
