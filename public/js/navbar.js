3f2a7e7a14810ddac72797beb7491059941237a7
Nora Shala <norashala00@gmail.com>
2024-04-18 14:08:16 -0400

    nav

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,3 +1,4 @@
+// Select all elements with the class 'nav-link' and iterate over them using forEach
 document.querySelectorAll('.nav-link').forEach((link) => {
   if (link.href === window.location.href) {
     link.setAttribute('aria-current', 'page');
