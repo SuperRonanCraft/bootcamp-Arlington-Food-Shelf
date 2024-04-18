c426ad8e04e122e0a2bd71271cb81e0b1bdb62d4
Nora Shala <norashala00@gmail.com>
2024-04-18 10:51:32 -0400

    line 4 comment

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 // Importing the Express module and creating a router instance
 const router = require('express').Router();
+// Importing route handlers for user-related routes
 const userRoutes = require('./userRoutes');
 const inventoryRoute = require('./inventoryRoute');
 const orderRoute = require('./orderRoute');
