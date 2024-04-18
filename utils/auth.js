1879105736506ed9cbd759924c4195348e51ed79
Nora Shala <norashala00@gmail.com>
2024-04-18 14:13:24 -0400

    middleware

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,5 @@
+// Define a middleware function named auth with parameters req (request), res (response), and next (next middleware function)
+
 const auth = (req, res, next) => {
   if (!req.session.loggedIn) {
     return res.redirect('/login');
