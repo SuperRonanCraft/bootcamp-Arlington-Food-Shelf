012bdd32fba61a1d1144106457eee7ad26c3c009
RonanCraft <SuperRonanCraft@gmail.com>
2024-04-18 23:14:03 -0400

    more inventory seeds

--- seeds/inventoryData.js
+++ seeds/inventoryData.js
@@ -2,41 +2,73 @@ const { Inventory } = require('../models/index');
 
 const data = [
   {
-    name: 'Bean',
+    name: 'Beans',
+    description: 'Canned beans',
     picture: 'beans.jpg',
   },
   {
     name: 'Potato',
-    picture: 'potatoes.jpg',
+    description: 'Fresh Idaho potatoes',
+    picture: 'potato.jpg',
   },
   {
     name: 'Lemon',
+    description: 'Fresh picked and bagged',
     picture: 'lemon.jpg',
   },
   {
-    name: 'Pizza',
-    name: 'Cheese',
+    name: 'Chedder Cheese',
     stock: '13',
     description: '1lb block of chedder cheese.',
-    picture: 'pizza.webp',
+    picture: 'chedder.jpg',
   },
   {
-    name: 'Frozen corn',
+    name: 'Corn on the Cob',
     stock: '10',
-    description: '2lb bag of frozen corn.',
+    description: '2lb bag of frozen',
     picture: 'corn.webp',
   },
   {
     name: 'Chicken',
     stock: '2',
-    description: 'Whole frozen chicken.',
+    description: 'Whole 1lb frozen chicken',
     picture: 'chicken.jpg',
   },
   {
     name: 'Lettuce',
     stock: '6',
-    description: 'Head of iceberg lettuce.',
+    description: 'Head of iceberg lettuce',
     picture: 'lettace_.jpg',
+  },
+  {
+    name: 'Banana',
+    stock: '6',
+    description: 'Packed in bundles of 5 or more',
+    picture: 'banana.png',
+  },
+  {
+    name: 'Tomatoe',
+    stock: '6',
+    description: 'Locally picked and fresh 1lb bag',
+    picture: 'tomatoe.png',
+  },
+  {
+    name: 'Bread',
+    stock: '6',
+    description: 'Sliced white sandwich bread',
+    picture: 'bread.png',
+  },
+  {
+    name: 'Ham',
+    stock: '6',
+    description: 'Freshly packed from the deli',
+    picture: 'ham.png',
+  },
+  {
+    name: 'Spaghetti',
+    stock: '6',
+    description: 'Packets of 16oz boxes',
+    picture: 'spaghetti.png',
   },
 ];
 
